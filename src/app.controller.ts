e62e2fe706da9e36ba1a2a82736222ef44f413f3
juancolchete <juanudkp@gmail.com>
2024-06-26 17:58:37 +0000

    feat: send request with params and headers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query,Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async apiMock(@Query() query:any): Promise<string> {
+    return await this.appService.apiMock(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import getfy from 'getfy';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async apiMock(query:any): Promise<string> {
+    const queryKeys = Object.keys(query)
+    const queryValues = Object.values(query)
+    let url = `${query.url}?`
+    for(let i =0;i<queryKeys.length;i++){
+      if(queryKeys[i] != "url"){
+        if(i == queryKeys.length -1){
+          url += `${queryKeys[i]}=${queryValues[i]}`  
+        }else{
+          url += `${queryKeys[i]}=${queryValues[i]}&`
+        }
+      }
+    }
+    await getfy(url)   
+    return 'ok';
   }
 }
